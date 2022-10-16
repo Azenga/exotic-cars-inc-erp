0c258747eb80e05cb260653177089f906daf46f1
Azenga Kevin <toomuch573@gmail.com>
2022-10-16 08:46:38 +0300

    WIP: Setting up area seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             CarSeeder::class,
             ServiceSeeder::class,
             LocationSeeder::class,
+            AreaSeeder::class,
         ]);
     }
 }
